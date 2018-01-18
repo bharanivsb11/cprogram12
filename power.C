f4185882062dbb210effb70b1adb002ef6137819
bharanivsb11 <35220778+bharanivsb11@users.noreply.github.com>
2018-01-18 06:33:54 -0800

    power.C

---
+++ power.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int base,exponent,k,power=1;
+scanf("%d\n",&base);
+scanf("%d\n",exponent);
+for(i=1;i<exponent;i++)
+{
+power = power*base
+}
+printf("power of number %d is %d",base,power);
+return 0;
+}
